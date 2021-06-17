e6efb936b863ea79333aa34d66b93c02e3e03f59
Moritz Geiger <moritzgeiger@hotmail.de>
2021-06-17 19:07:10 +0200

    beautifying send_mail

--- send_email.py
+++ send_email.py
@@ -7,23 +7,31 @@ from dotenv import load_dotenv, find_dotenv
 import os
 from datetime import datetime
 
+homename = 'Bunkuhome'
 
 def load_args():
+	"""
+	loads all the default arguments for the send_email() function. 
+	some arguments are predefined, some can be set through a terminal input
+	"""
 	load_dotenv(find_dotenv())
 	GMAIL = os.environ.get("GMAIL")
 	port = 465  # For SSL
 	#password = input("Type your password and press enter: ")
-	sender_email = "bunkuhome@gmail.com"
+	sender_email = os.environ.get("SENDER")
 	receiver_email = input("Send email to: ")
 	now = datetime.now().strftime("%y-%m-%d %H:%M:%S")
-	subject = f"Subject: Bunkuhome stats at {now}\n"
+	subject = f"Subject: {homename} stats at {now}\n"
 	signature = "\n\nsent to you with love from raspberry"
 	return port, GMAIL, sender_email, receiver_email, subject, signature
 	
 
 def get_temp():
+	"""
+	returns the current temperature of the sense hat wrapped in a descriptive string
+	"""
 	print("get_temp was called")
-	string = '\nThe current temperature in Bunkuhome is: '
+	string = f'\nThe current temperature in {homename} is: '
 	sense = SenseHat()
 	sense.clear()
 	temp = sense.get_temperature()
@@ -31,8 +39,11 @@ def get_temp():
 	return string + str(round(temp, 2)) + "*C"
 	
 def get_hum():
+	"""
+	returns the current humidity of the sense hat wrapped in a descriptive string
+	"""
 	print("get_hum was called")
-	string = '\nThe current humidity in Bunkuhome is: '
+	string = f'\nThe current humidity in {homename} is: '
 	sense = SenseHat()
 	hum = sense.get_humidity()
 	print(f"returning {string + str(round(hum, 2))} %")
@@ -42,7 +53,13 @@ def send_mail(port,
 			password,  
 			receiver_email,
 			message,
-			sender_email = "bunkuhome@gmail.com"):
+			sender_email):
+	"""
+	takes in arguments neccessary to send an email.
+	args: port, password, receiver_email, message, sender_email
+	
+	initiates sending of an email.
+	"""
 	# Create a secure SSL context
 	context = ssl.create_default_context()
 
@@ -54,11 +71,13 @@ if __name__ == '__main__':
 	# get email stuff
 	port, GMAIL, sender_email, receiver_email, subject, signature = load_args()
 	
+	# get stats
 	message = get_temp() + get_hum()
 	
 	send_mail(port=port, 
 			password=GMAIL, 
 			receiver_email=receiver_email,
-			message=subject+message+signature)
+			message=subject+message+signature, # compile message
+			sender_email=sender_email)
 	print(f"email sent to {receiver_email}")
 	print(f"final message: {subject+message+signature}")
